dabcf72304952329e4732b374df85c316ac35f78
Andre Steingress <me@andresteingress.com>
2014-01-31 10:59:32 +0100

    deactivated actorClosure for now

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,17 +1,4 @@
-import org.grails.haudit.AuditLogListener
-import org.grails.haudit.AuditLogListenerUtil
-import org.grails.haudit.AuditLogEvent
+import grails.plugin.hibernateaudit.HibernateAuditLogPluginSupport
 
 // Place your Spring DSL code here
-beans = {
-    auditLogListener(AuditLogListener, ref('hibernateDatastore'))  {
-        grailsApplication = ref('grailsApplication')
-        sessionFactory = ref('sessionFactory')
-
-        sessionAttribute = grailsApplication.config.auditLog.sessionAttribute ?: ""
-        actorKey = grailsApplication.config.auditLog.actorKey ?: ""
-        truncateLength = grailsApplication.config.auditLog.truncateLength ?: AuditLogEvent.MAX_SIZE
-        actorClosure = grailsApplication.config.auditLog.actorClosure ?: AuditLogListenerUtil.actorDefaultGetter
-        defaultIgnoreList = grailsApplication.config.auditLog.defaultIgnore?.asImmutable() ?: ['version', 'lastUpdated'].asImmutable()
-    }
-}
+beans = HibernateAuditLogPluginSupport.doWithSpring
--- src/groovy/grails/plugin/hibernateaudit/HibernateAuditLogPluginSupport.groovy
+++ src/groovy/grails/plugin/hibernateaudit/HibernateAuditLogPluginSupport.groovy
@@ -15,7 +15,7 @@ class HibernateAuditLogPluginSupport {
             sessionAttribute = grailsApplication.config.auditLog.sessionAttribute ?: ""
             actorKey = grailsApplication.config.auditLog.actorKey ?: ""
             truncateLength = grailsApplication.config.auditLog.truncateLength ?: AuditLogEvent.MAX_SIZE
-            actorClosure = grailsApplication.config.auditLog.actorClosure ?: AuditLogListenerUtil.actorDefaultGetter
+            // actorClosure = grailsApplication.config.auditLog.actorClosure ?: AuditLogListenerUtil.actorDefaultGetter
             defaultIgnoreList = grailsApplication.config.auditLog.defaultIgnore?.asImmutable() ?: ['version', 'lastUpdated'].asImmutable()
         }
     }
