7823a46e9fe92748f9ab079bd84164f7ff77b03f
Andre Steingress <me@andresteingress.com>
2014-03-25 20:23:09 +0100

    version bump

--- HibernateAuditLogGrailsPlugin.groovy
+++ HibernateAuditLogGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugin.hibernateaudit.HibernateAuditLogPluginSupport
  */
 class HibernateAuditLogGrailsPlugin {
 
-    def version = "0.9-SNAPSHOT"
+    def version = "0.10-SNAPSHOT"
     def grailsVersion = "2.1 > *"
 
     // resources that are excluded from plugin packaging
